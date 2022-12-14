70e65fb2879b744b9baeaf303c0b0c590fa9aa68
Milena Erbez <erbez01@gmail.com>
2022-12-14 18:48:45 +0100

    NavBar

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
 
-const prviElement=<h1>Hello world!</h1>;
+//const prviElement=<h1>Hello world!</h1>;
 ReactDOM.render(
-  prviElement,
+  <App/>,
   document.getElementById("root")
 );
---
+++ src/navBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className="navBar">
+        <a>My store</a>
+        </div>
+  )
+}
+
+export default NavBar
